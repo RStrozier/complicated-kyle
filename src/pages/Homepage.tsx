765d545c2e5f4c326a40b4e53fe4f474d7223e3b
Strozier <strozierrashida@vineyardcolumbus.org>
2024-09-11 15:42:32 -0400

    added bootstrap and tailwind set up basics

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,12 @@
 const Homepage = () => {
   return (
     <>
-     <div className="div">Hi test what is actually happening?</div>
+    <div className="container-md">
+    <div className="mx-10">This is a test side project to describe the 
+        complications of my boss, Kyle. We are working on making this a 
+        lighthearted project, filled with laughs and smiles.
+     </div>
+    </div>
     </>
   )
 }
