e6beffa4a52e8413639ba3f3fb600d7535e902fa
Strozier <strozierrashida@vineyardcolumbus.org>
2024-09-24 15:20:58 -0400

    final draft hopefully...

--- src/sections/SectionFour.tsx
+++ src/sections/SectionFour.tsx
@@ -1,24 +1,17 @@
 import ComplicatedMeme from "../assets/memes/complicated-meme.jpg";
-import CSSCaptainMeme from "../assets/memes/css-captain-now-meme.jpg";
 import ChangeMindMeme from "../assets/memes/change-my-mind-meme.jpg";
 
 const SectionFour = () => {
   return (
    <>
        <div className="bg-sub-col-2 h-[800px] flex flex-col justify-center items-center">
-            <img src={ComplicatedMeme}></img>
-            <div className="flex flex-col justify-center items-center container-md">
-              <img
+            <img src={ComplicatedMeme}
+            className="h-[60%] w-auto"
+            ></img>
+            <img
                 src={ChangeMindMeme}
-                className="w-[50%] h-auto"
-                alt="Change Mind Meme"
+                    className="h-[60%] w-auto"
               />
-              <img
-                src={CSSCaptainMeme}
-                className="w-[50%] h-auto"
-                alt="CSS Captain Meme"
-              />
-            </div>
           </div>
    </>
   )
--- src/sections/SectionOne.tsx
+++ src/sections/SectionOne.tsx
@@ -1,13 +1,17 @@
-import HomepageHero from "../components/HomepageHero"
+import HomepageHero from "../components/HomepageHero";
+import CSSCaptainMeme from "../assets/memes/css-captain-now-meme.jpg";
 
 const SectionOne = () => {
   return (
     <>
-     <div className="bg-main-col h-[300px]">
-            <HomepageHero />
-          </div>
+      <div className="bg-main-col h-[300px]">
+        <HomepageHero />
+      </div>
+      <div className="flex justify-center items-center">
+        <img src={CSSCaptainMeme}></img>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionOne
+export default SectionOne;
