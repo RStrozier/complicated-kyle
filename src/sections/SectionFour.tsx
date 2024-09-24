5f9326a515a638ea391a14bcc46f55b04cc18056
Strozier <strozierrashida@vineyardcolumbus.org>
2024-09-24 15:12:23 -0400

    pushing final changes

--- src/sections/SectionFour.tsx
+++ src/sections/SectionFour.tsx
@@ -1,10 +1,24 @@
 import ComplicatedMeme from "../assets/memes/complicated-meme.jpg";
+import CSSCaptainMeme from "../assets/memes/css-captain-now-meme.jpg";
+import ChangeMindMeme from "../assets/memes/change-my-mind-meme.jpg";
 
 const SectionFour = () => {
   return (
    <>
        <div className="bg-sub-col-2 h-[800px] flex flex-col justify-center items-center">
             <img src={ComplicatedMeme}></img>
+            <div className="flex flex-col justify-center items-center container-md">
+              <img
+                src={ChangeMindMeme}
+                className="w-[50%] h-auto"
+                alt="Change Mind Meme"
+              />
+              <img
+                src={CSSCaptainMeme}
+                className="w-[50%] h-auto"
+                alt="CSS Captain Meme"
+              />
+            </div>
           </div>
    </>
   )
--- src/sections/SectionOne.tsx
+++ src/sections/SectionOne.tsx
@@ -1,24 +1,10 @@
 import HomepageHero from "../components/HomepageHero"
-import CSSCaptainMeme from "../assets/memes/css-captain-now-meme.jpg";
-import ChangeMindMeme from "../assets/memes/change-my-mind-meme.jpg";
 
 const SectionOne = () => {
   return (
     <>
-     <div className="bg-main-col h-[400px]">
+     <div className="bg-main-col h-[300px]">
             <HomepageHero />
-            <div className="flex justify-center items-center container-md">
-              <img
-                src={ChangeMindMeme}
-                className="w-auto h-[10rem] mx-2"
-                alt="Change Mind Meme"
-              />
-              <img
-                src={CSSCaptainMeme}
-                className="w-auto h-[10rem] mx-2"
-                alt="CSS Captain Meme"
-              />
-            </div>
           </div>
     </>
   )
